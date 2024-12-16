83d44d3168023ef2e05f712b784fb966d80b7400
Mark Cruz <cruzm430@gmail.com>
2024-12-16 08:27:09 -0500

    added shot

--- circleshape.py
+++ circleshape.py
@@ -1,22 +1,123 @@
+asteroid.py
+U
+
+asteroidfield.py
+U
+
+circleshape.py
+U
+
+constants.py
+M
+
+main.py
+M
+
+player.py
+M
+
+requirements.txt
+U
+
+shot.py
+A
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
 import pygame
+
 
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
-
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
     def draw(self, screen):
-        # sub-classes must override
+        # must override
         pass
 
     def update(self, dt):
-        # sub-classes must override
+        # must override
         pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+import pygame
+
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # must override
+        pass
+
+    def update(self, dt):
+        # must override
+        pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
+
